57674aece56abc9f08fe013d5a33c5217917ff90
student <patnala.sowmya@hcl.com>
2021-04-20 18:23:36 +0000

    first commit

---
+++ GradedAssignment/src/main/java/com/sowmya/GradedAssignment/App.java
@@ -0,0 +1,13 @@
+package com.sowmya.GradedAssignment;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Dev-Ops" );
+    }
+}
